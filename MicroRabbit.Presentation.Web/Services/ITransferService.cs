06d489e144a229d10154deef39d432766c7360e5
= <Israel.arauj0@live.com>
2020-06-24 21:48:47 +0100

    aula 72

---
+++ MicroRabbit.Presentation.Web/Services/ITransferService.cs
@@ -0,0 +1,10 @@
+﻿using MicroRabbit.Presentation.Web.Models.DTO;
+using System.Threading.Tasks;
+
+namespace MicroRabbit.Presentation.Web.Services
+{
+    public interface ITransferService
+    {
+        Task Transfer(TransferDTO transferDTO);
+    }
+}
---
+++ MicroRabbit.Presentation.Web/Services/TransferService.cs
@@ -0,0 +1,33 @@
+﻿using MicroRabbit.Presentation.Web.Models.DTO;
+using Microsoft.Extensions.DependencyInjection;
+using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Text;
+using System.Text.Json.Serialization;
+using System.Threading.Tasks;
+
+namespace MicroRabbit.Presentation.Web.Services
+{
+    public class TransferService : ITransferService
+    {
+        private readonly HttpClient _apiClient;
+
+        public TransferService(HttpClient apiClient)
+        {
+            _apiClient = apiClient;
+        }
+
+        public async Task Transfer(TransferDTO transferDTO)
+        {
+            var uri = "https://localhost:5001/api/banking";
+            var json = JsonConvert.SerializeObject(transferDTO);
+            var transferContent = new StringContent(json, Encoding.UTF8, "application/json");
+
+            var response = await _apiClient.PostAsync(uri, transferContent);
+            response.EnsureSuccessStatusCode();
+        }
+    }
+}
