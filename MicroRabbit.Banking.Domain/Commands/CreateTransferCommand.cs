f19118229516a03a9c9e38488ea22683ad044037
= <Israel.arauj0@live.com>
2020-06-24 16:16:25 +0100

    aula 46

---
+++ MicroRabbit.Banking.Domain/Commands/CreateTransferCommand.cs
@@ -0,0 +1,12 @@
+﻿namespace MicroRabbit.Banking.Domain.Commands
+{
+    public abstract class CreateTransferCommand : TransferCommand
+    {
+        public CreateTransferCommand(int from, int to, decimal amount)
+        {
+            From = from;
+            To = to;
+            Amount = amount;
+        }
+    }
+}
