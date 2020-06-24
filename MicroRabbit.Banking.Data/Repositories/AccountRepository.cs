dadb29e1d810d1ea59bfe98adaaced8180b53a3c
= <Israel.arauj0@live.com>
2020-06-24 12:23:58 +0100

    aula 36

---
+++ MicroRabbit.Banking.Data/Repositories/AccountRepository.cs
@@ -0,0 +1,23 @@
+﻿using MicroRabbit.Banking.Data.Contexts;
+using MicroRabbit.Banking.Domain.Interfaces;
+using MicroRabbit.Banking.Domain.Models;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace MicroRabbit.Banking.Data.Repositories
+{
+    public class AccountRepository : IAccountRepository
+    {
+        private readonly BankingDbContext _ctx;
+
+        public AccountRepository(BankingDbContext context)
+        {
+            _ctx = context;
+        }
+
+        public IEnumerable<Account> GetAccounts()
+        {
+            return _ctx.Accounts.ToList();
+        }
+    }
+}
