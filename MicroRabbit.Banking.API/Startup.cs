81039d56253ca9250fd67c4698750b1dc94233ab
= <Israel.arauj0@live.com>
2020-06-24 12:49:13 +0100

    startup edit

--- MicroRabbit.Banking.API/Startup.cs
+++ MicroRabbit.Banking.API/Startup.cs
@@ -2,11 +2,14 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using MicroRabbit.Banking.Data.Contexts;
+using MicroRabbit.Banking.Domain.Models;
 using MicroRabbit.Infra.IoC;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.HttpsPolicy;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
@@ -26,6 +29,8 @@ namespace MicroRabbit.Banking.API
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
+            services.AddDbContext<BankingDbContext>(options => options.UseSqlServer(Configuration.GetConnectionString("BankingDbConnection")));
+
             services.AddControllers();
 
             DependencyContainer.RegisterIoC(services);
