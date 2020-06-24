3ded39e45cf2c4e6f6b6c567d9c6c7ae4381f1a7
= <Israel.arauj0@live.com>
2020-06-24 18:47:06 +0100

    aula 61

---
+++ MicroRabbit.Transfer.API/Controllers/TransferController.cs
@@ -0,0 +1,27 @@
+﻿using MicroRabbit.Transfer.Application.Interfaces;
+using MicroRabbit.Transfer.Domain.Models;
+using Microsoft.AspNetCore.Mvc;
+using System.Collections.Generic;
+
+namespace MicroRabbit.Transfer.API.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class TransferController : ControllerBase
+    {
+        private readonly ITransferService _transferService;
+
+        public TransferController(ITransferService transferService)
+        {
+            _transferService = transferService;
+        }
+
+        // GET api/transfer
+        [HttpGet]
+        public ActionResult<IEnumerable<TransferLog>> Get()
+        {
+            var transfers = _transferService.GetTransferLogs();
+            return Ok(transfers);
+        }
+    }
+}
