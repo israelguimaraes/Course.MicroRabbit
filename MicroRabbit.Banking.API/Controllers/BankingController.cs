87d5ecbbdb17f89457fcf7847a5bba386a014633
= <Israel.arauj0@live.com>
2020-06-24 13:10:43 +0100

    aula 41

---
+++ MicroRabbit.Banking.API/Controllers/BankingController.cs
@@ -0,0 +1,27 @@
+﻿using MicroRabbit.Banking.Application.Interfaces;
+using MicroRabbit.Banking.Domain.Models;
+using Microsoft.AspNetCore.Mvc;
+using System.Collections.Generic;
+
+namespace MicroRabbit.Banking.API.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class BankingController : ControllerBase
+    {
+        private readonly IAccountService _accountService;
+
+        public BankingController(IAccountService accountService)
+        {
+            _accountService = accountService;
+        }
+
+        // GET api/banking
+        [HttpGet]
+        public ActionResult<IEnumerable<Account>> Get()
+        {
+            var accounts = _accountService.GetAccounts();
+            return Ok(accounts);
+        }
+    }
+}
