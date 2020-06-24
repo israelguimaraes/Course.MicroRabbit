44cf5d52677950df28f7f6c6d1289dbb4fa9789e
= <Israel.arauj0@live.com>
2020-06-24 19:11:11 +0100

    aula 63

---
+++ MicroRabbit.Transfer.Domain/EventHandlers/TransferEventHandler.cs
@@ -0,0 +1,27 @@
+﻿using MicroRabbit.Domain.Core.Bus;
+using MicroRabbit.Transfer.Domain.Events;
+using MicroRabbit.Transfer.Domain.Models;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MicroRabbit.Transfer.Domain.EventHandlers
+{
+    public class TransferEventHandler : IEventHandler<TransferCreatedEvent>
+    {
+        private readonly IEventBus _bus;
+
+        public TransferEventHandler(IEventBus bus)
+        {
+            _bus = bus;
+        }
+
+        public Task Handle(TransferCreatedEvent @event)
+        {
+
+
+            return Task.CompletedTask;
+        }
+    }
+}
