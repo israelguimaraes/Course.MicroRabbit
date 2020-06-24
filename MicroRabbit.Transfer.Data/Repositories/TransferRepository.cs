7e0070483c807f2df2bf0899b91e6f57ebec1b42
= <Israel.arauj0@live.com>
2020-06-24 18:33:06 +0100

    aula 59

---
+++ MicroRabbit.Transfer.Data/Repositories/TransferRepository.cs
@@ -0,0 +1,23 @@
+﻿using MicroRabbit.Transfer.Data.Contexts;
+using MicroRabbit.Transfer.Domain.Interfaces;
+using MicroRabbit.Transfer.Domain.Models;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace MicroRabbit.Transfer.Data.Repositories
+{
+    public class TransferRepository : ITransferRepository
+    {
+        private readonly TransferDbContext _ctx;
+
+        public TransferRepository(TransferDbContext ctx)
+        {
+            _ctx = ctx;
+        }
+
+        public IEnumerable<TransferLog> GetTransferLogs()
+        {
+            return _ctx.Transfers.ToList();
+        }
+    }
+}
